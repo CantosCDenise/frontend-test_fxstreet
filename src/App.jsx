a921d0812e18fb04c1718f937fcdd10e6c1f66bb
Denise Cantos <142037050+CantosCDenise@users.noreply.github.com>
2024-06-16 23:51:14 +0200

    latest vs popular using useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import AppContainer from "./Components/AppContainer";
 import MainContent from "./Components/MainContent";
-import MCPopular from "./Components/MCPopular"
+import MCPopular from "./Components/MCPopular";
 import Sidebar from "./Components/Sidebar";
 import GlobalStyles from "./styles/GlobalStyles";
-import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <AppContainer>
-      <GlobalStyles />
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<MainContent />} />
-      </Routes>
-    </AppContainer>
+      <AppContainer>
+        <GlobalStyles />
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<MainContent />} />
+          <Route path="/popular" element={<MCPopular />} />
+        </Routes>
+      </AppContainer>
   );
 }
 
--- src/Components/MainContent.jsx
+++ src/Components/MainContent.jsx
@@ -1,31 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Navbar from "./Navbar";
 import CardPost from "./CardPost";
 import Latest from "./Latest";
 import { useNavigate } from "react-router-dom";
-import Content from "./Content";
-import Popular from "./Popular";
-
 
 const MainContentContainer = styled.div`
     display: flex;
     width: 100%;
     flex-direction: column;
     background-color: #ececf1;
-`
+`;
 
 const MainContent = () => {
-   const [view, setView] = useState("latest");
+    const navigate = useNavigate();
+
+    function handleLatestButtonClick() {
+        navigate("/");
+    }
+
+    function handlePopularButtonClick() {
+        navigate("/popular");
+    }
 
     return (
         <MainContentContainer>
             <Navbar />
-            <button onClick={() => setView("latest")}>Latest</button>
-            <button onClick={() => setView("popular")}>Popular</button>
-            {view === "latest" ? <Latest /> : <Popular />}
+            <button onClick={handleLatestButtonClick}>Latest</button>
+            <button onClick={handlePopularButtonClick}>Popular</button>
+            <Latest /> {/* Opcional: Puedes renderizar Latest directamente */}
         </MainContentContainer>
-        
     );
 };
 
